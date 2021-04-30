676e4599932e702456758f72d6c8e4f90997ce40
r-o-n-c <hsingjungc@student.unimelb.edu.au>
2021-04-30 22:44:22 +0800

    Get the token from the mock server.

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,6 +19,13 @@ export const Login = () => {
                 password: ''
             }}
             validationSchema={validate}
+            onSubmit={values => {
+                fetch('/api/v2/users/tokens',
+                    {
+                        method: 'POST',
+                        body: JSON.stringify(values)
+                    })
+            }}
         >
             {formik => (
                 <div>
@@ -26,7 +33,7 @@ export const Login = () => {
                     <Form>
                         <TextField label="Email" name="email" type="email"/>
                         <TextField label="password" name="password" type="password"/>
-                        <button className="btn btn-dark mt-3" type="submit">Login</button>
+                        <button className="btn btn-dark mt-3" type="submit" disabled={!formik.isValid}>Login</button>
                     </Form>
                 </div>
             )}
